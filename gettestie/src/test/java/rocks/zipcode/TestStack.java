5f172632a867bd4294e9a3784869f0851caba326
Raheel Uppal <ruppal@zipcodes-MBP.fios-router.home>
2020-03-01 20:02:54 -0500

    stack

--- gettestie/src/test/java/rocks/zipcode/TestStack.java
+++ gettestie/src/test/java/rocks/zipcode/TestStack.java
@@ -1,9 +1,15 @@
 package rocks.zipcode;
 
+import gettestie.src.main.java.rocks.zipcode.Person;
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 import java.util.Stack;
+import java.util.logging.Logger;
 
 public class TestStack {
+    private static final Logger LOGGER = Logger.getLogger(TestStack.class.getName());
 
     @org.junit.Before
     public void setUp() throws Exception {
@@ -19,6 +25,36 @@ public class TestStack {
         stack.push("Hello world");
         assertEquals(false, stack.isEmpty()); // false
     }
-
-    // Make a bigger test exercising more Stack methods.....
+    @Test
+    public void TestStack2(){
+        Stack<Person> stack = new Stack<>();
+        Person Raheel = new Person("Raheel",1994);
+        Person Zan = new Person("Zan",1991);
+        stack.push(Raheel);
+        stack.push(Zan);
+        Person expected = Zan;
+        Person actual = stack.pop();
+        LOGGER.info("" + actual);
+        Assert.assertEquals(expected,actual);
+    }
+    @Test
+    public void TestStack3(){
+        Stack<Person> stack = new Stack<>();
+        Person Raheel = new Person("Raheel",1994);
+        Person Zan = new Person("Zan",1991);
+        stack.push(Raheel);
+        stack.push(Zan);
+        Person expected = Zan;
+        Person actual = stack.peek();
+        LOGGER.info("" + actual);
+        Assert.assertEquals(expected,actual);
+    }
+    @Test
+    public void TestStack4(){
+        Stack<Person> stack = new Stack<>();
+        Person Raheel = new Person("Raheel",1994);
+        Person Zan = new Person("Zan",1991);
+        LOGGER.info("" + stack);
+        Assert.assertTrue(stack.empty());
+    }
 }
